7daef7b9b53e73a6dbb6d5c0054a4a988930fa1c
Tanner Grehawick <tgrehawi@gmail.com>
2017-07-15 16:29:19 -0400

    take 3

--- Assets/Scripts/PlayerHammerController.cs
+++ Assets/Scripts/PlayerHammerController.cs
@@ -6,21 +6,35 @@ public class PlayerHammerController : MonoBehaviour {
 
 	public Player player { get; private set; }
 
-	public float angularAcceleration = 30;
+	public float angularVelocityFactor = 5;
+
+	// public float angularAcceleration = 30;
+
+	bool swingRequested;
 
 	void Awake() {
 		player = GetComponent<Player>();
 	}
 
+	void Update() {
+		if (!swingRequested && Input.GetButtonDown("Fire1")) {
+			swingRequested = true;
+		}
+	}
 	void FixedUpdate() {
 		Hammer hammer = player.hammer;
 		if (hammer != null) {
-			
-
+			if (swingRequested) {
+				swingRequested = false;
+				Vector2 input = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+				Vector2 hammerDirection = hammer.transform.right;
+				float angle = Vector2.SignedAngle(hammerDirection, input);
+				hammer.body.angularVelocity = angle * angularVelocityFactor;
+			}
 			// TAKE 2
-			float torque = angularAcceleration * hammer.body.inertia;
-			float input = Input.GetAxisRaw("Horizontal");
-			hammer.body.AddTorque(- torque * input);
+			// float torque = angularAcceleration * hammer.body.inertia;
+			// float input = Input.GetAxisRaw("Horizontal");
+			// hammer.body.AddTorque(- torque * input);
 
 			// TAKE 1
 			// Vector2 input = new Vector2(
